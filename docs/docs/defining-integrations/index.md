9e5a6e8daf60747c1822a53d94c1c8722d0c47c8
Dipu <dipuchaurasiya91@gmail.com>
2024-05-11 14:07:46 +0545

    initital commit

---
+++ docs/docs/defining-integrations/index.md
@@ -0,0 +1,5 @@
+---
+title: Defining Integrations
+sidebar_label: Defining Integrations
+slug: /defining-integrations
+---
---
+++ docs/sidebars.ts
@@ -0,0 +1,37 @@
+import type { SidebarsConfig } from "@docusaurus/plugin-content-docs";
+
+/**
+ * Creating a sidebar enables you to:
+ - create an ordered group of docs
+ - render a sidebar for each doc of that group
+ - provide next/previous navigation
+
+ The sidebars can be generated from the filesystem, or explicitly defined here.
+
+ Create as many sidebars as you want.
+ */
+const sidebars: SidebarsConfig = {
+  docs: [
+    "index",
+    "quick-start",
+    {
+      type: "category",
+      label: "Defining-integrations",
+      collapsed: false,
+      items: [
+        "defining-integrations/index",
+        "defining-integrations/read-actions",
+        "defining-integrations/write-actions",
+        "defining-integrations/proxy-actions",
+        "defining-integrations/subscribe-actions",
+        "defining-integrations/destinations",
+      ],
+    },
+    "deploy-integrations",
+    "embed-ui-components",
+    "supported-apis",
+    "glossary",
+  ],
+};
+
+export default sidebars;
